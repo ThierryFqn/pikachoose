6483df3e3b9578a021a5b1b8a55d2f408f32e8e8
Thierry Fonquernie <thierry.fonquernie@gmail.com>
2022-02-22 11:52:41 +0100

    create and new methods done

--- app/policies/pokemon_policy.rb
+++ app/policies/pokemon_policy.rb
@@ -1,8 +1,16 @@
 class PokemonPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
-    # def resolve
-    #   scope.all
-    # end
+    def resolve
+      scope.all
+    end
+  end
+
+  def create?
+    true
+  end
+
+  def new?
+    create?
   end
 end
