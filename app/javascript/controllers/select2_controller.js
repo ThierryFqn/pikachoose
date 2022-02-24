ddf1251f0ff706aedbb5f18046389fd4a8781ca4
Thierry Fonquernie <thierry.fonquernie@gmail.com>
2022-02-24 18:17:15 +0100

    first debugg

--- app/javascript/controllers/select2_controller.js
+++ app/javascript/controllers/select2_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus";
 import $ from "jquery";
 import "select2";
-// import 'select2/dist/css/select2.min.css'; <- Uncomment this line if you are using Rails
+import 'select2/dist/css/select2.min.css';
 
 export default class extends Controller {
   connect() {
